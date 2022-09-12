d9bb81929f314b6db45fca5eb03de84baead1af0
Maicon <105327553+maiconlara@users.noreply.github.com>
2022-09-12 17:41:07 -0300

    Add files via upload

---
+++ produtos/produtos.js
@@ -0,0 +1,65 @@
+let produtoJson = [
+    {
+        id: 1,
+        name: 'PROCESSADOR AMD RYZEN 5 4600G 3.7GHZ',
+        img: 'images/processador.png',
+        price: [,,879.00],
+        description: 'Processador AMD Ryzen 5 4600G 3.7GHz (4.2GHz Turbo), 6-Cores 12-Threads, Cooler Wraith Stealth, AM4, 100-100000147BOX'
+      },
+      {
+        id: 2,
+        name: 'SSD KINGSTON NV1, 500GB',
+        img: 'images/ssd.png',
+        price: [,, 299.00],
+        description: 'SSD Kingston NV1, 500GB, M.2 NVMe, 2280, Leitura 2100MBs e Gravação 1700MBs, SNVS/500G.'
+      },
+      {
+        id: 3,
+        name: 'GABINETE GAMER T-DAGGER, CUBE',
+        img: 'images/gabinete.png',
+        price: [,, 229.90],
+        description: 'Gabinete Gamer T-Dagger, Cube, Mid Tower, Vidro Temperado, Black, Sem Fonte, Sem Fan'
+      },
+      {
+        id: 4,
+        name: 'COMBO GAMER REDRAGON',
+        img: 'images/combo.png',
+        price: [,, 309.90],
+        description: 'Combo Gamer Redragon Teclado Mecanico Rainbow e Mouse RGB, S108 Light Green'
+      },
+      {
+        id: 5,
+        name: 'FONTE REDRAGON RGPS 500W',
+        img: 'images/fonte.png',
+        price: [,, 279.90],
+        description: 'Fonte Redragon RGPS 500W, 80 Plus Bronze, PFC Ativo, GC-PS001'
+      },
+      {
+        id: 6,
+        name: 'PLACA MÃE GIGABYTE B550M AORUS ELITE',
+        img: 'images/placa.png',
+        price: [,, 899.00],
+        description: 'Placa Mãe Gigabyte B550M AORUS Elite, Chipset B550, AMD AM4, mATX, DDR4'
+      },
+      {
+        id: 7,
+        name: 'SSD KINGSTON A400 2,5 SATA III 240GB',
+        img: 'images/ssd2.png',
+        price: [,, 159.99],
+        description: 'SSD Kingston A400, 240GB, Sata III, Leitura 500MBs Gravação 350MBs, SA400S37/240G'
+      },
+      {
+        id: 8,
+        name: 'MONITOR PCTOP LED, 18.5 POL',
+        img: 'images/monitor.png',
+        price: [,, 429.00],
+        description: 'Monitor Gamer PCTop LED, 18.5 Pol, 60Hz, HDMI/VGA, VESA, Com Inclinação, MLP185HDMI'
+      },
+      {
+        id: 9,
+        name: 'HEADSET GAMER REDRAGON MINOS H210',
+        img: 'images/headset.png',
+        price: [,, 429.00],
+        description: 'Headset Gamer Redragon Minos H210, Surround 7.1, USB, Preto/Vermelho'
+      }
+];
---
+++ produtos/produtos.php
@@ -0,0 +1,141 @@
+<?php
+require "../db_functions.php";
+require "../force_authenticate.php";
+require "../db_credentials.php";
+
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (isset($_POST["pay"])) {
+	   $conn = connect_db();
+	   //$pay = mysqli_real_escape_string($conn,$_POST["pay"]);
+	   $sql = "INSERT INTO $table_prods
+              (emailUser, total) VALUES
+              ('".$_SESSION["user_email"]."', 50);";
+			  $result = mysqli_query($conn, $sql);
+  }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Produtos</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+
+
+    <div class="models">
+
+
+        <div class="produto-item">
+            <a href="">
+                <div class="produto-item--img"><img src="" /></div>
+                <div class="produto-item--add">+</div>
+            </a>
+            <div class="produto-item--price">R$ --</div>
+            <div class="produto-item--name">--</div>
+            <div class="produto-item--desc">--</div>
+        </div>
+
+
+        <div class="cart--item">
+            <img src="" />
+            <div class="cart--item-nome">--</div>
+            <div class="cart--item--qtarea">
+                <button class="cart--item-qtmenos">-</button>
+                <div class="cart--item--qt">1</div>
+                <button class="cart--item-qtmais">+</button>
+            </div>
+        </div>
+
+    </div>
+
+    <header>
+        <img class="logo" src="images/logopng.png" alt="logo">
+        <nav>
+            <ul class="nav-link">
+                <li><a href="/Login/inicio/homepage.php">Página Inicial</a></li>
+                <li><a href="/Login/produtos/produtos.php">Produtos</a></li>
+                <li><a href="/Login/recibo/recibo.php">Histórico</a></li>
+                <li><a href="/Login/about/about.php">Sobre</a></li>
+            </ul>
+        </nav>
+      
+        <div class="menu-openner"><span>0</span>🛒</div>
+        <button id="contact-button" onclick="mostrar()">Conta &#x1F464;</button>
+        <ul id="conta-options">
+                <li><a href="/Login/conta/conta.php">Alterar Dados</a></li>
+                <li><a href="/Login/login.php">Sair &rarr;</a></li>
+        </ul> 
+    </header>
+
+    <main>
+        
+        <h2>Produtos</h2>
+
+        <div class="produto-area"></div>
+    </main>
+
+    <aside>
+        <div class="cart--area">
+            <div class="menu-closer">❌</div>
+            <h1>Seu Carrinho</h1>
+            <div class="cart"></div>
+            <div class="cart--details">
+                <div class="cart--totalitem subtotal">
+                    <span>Subtotal</span>
+                    <span>R$ --</span>
+                </div>
+                <div class="cart--totalitem desconto">
+                    <span>Desconto</span>
+                    <span>R$ --</span>
+                </div>
+                <div class="cart--totalitem total big">
+                    <span>Total</span>
+                    <form name="pay" action="" method="post">
+					<span>R$ --</span>
+					</form>
+                </div>
+                <form action="" method="post">
+                <div class="cart--finalizar">
+				<input type="submit" name="pay" value="Finalizar a Compra" class="modal-btn"></div>
+				</form>
+            </div>
+        </div>
+    </aside>
+
+    <div class="produtoWindowArea">
+        <div class="produtoWindowBody">
+            <div class="produtoInfo--cancelMobileButton">Voltar</div>
+            <div class="produtoBig">
+                <img src="" />
+            </div>
+            <div class="produtoInfo">
+                <h1>--</h1>
+                <div class="produtoInfo--desc">--</div>
+                <div class="produtoInfo--pricearea">
+                    <div class="produtoInfo--sector">Preço</div>
+                    <div class="produtoInfo--price">
+                        <div class="produtoInfo--actualPrice">R$ --</div>
+                        <div class="produtoInfo--qtarea">
+                            <button class="produtoInfo--qtmenos">-</button>
+                            <div class="produtoInfo--qt">1</div>
+                            <button class="produtoInfo--qtmais">+</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="produtoInfo--addButton">Adicionar ao carrinho</div>
+                <div class="produtoInfo--cancelButton">Cancelar</div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="produtos.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
